84046ef289110d0124b46ad7e3899cf38f229e06
afif julio <afifjulio28@gmail.com>
2019-12-17 18:56:47 +0700

    revisi

--- exercise3.js
+++ exercise3.js
@@ -1,21 +1,27 @@
+//Input nama dan peran
 var nama = "Afif";
 var peran = "";
 
+//jika nama tidak sama dengan kosong dan value peran sama dengan kosong
 if(nama !== "" && peran === ""){
 console.log(`Halo ${nama}, pilih peranmu untuk memulai game!`);
 }
+//jika nama tidak sama dengan kosong dan value peran sama dengan ksatria
 else if(nama !== "" & peran === "Ksatria"){
 console.log(`Selamat datang di Dunia Proxytia, ${nama}`)
 console.log(`Halo Ksatria ${nama}, kamu dapat menyerang dengan senjatamu!`)
 }
+//jika nama tidak sama dengan kosong dan value peran sama dengan tabib
 else if(nama !== "" & peran === "Tabib"){
 console.log(`Selamat datang di Dunia Proxytia, ${nama}`)
 console.log(`Halo Tabib ${nama}, kamu akan membantu temanmu yang terluka.`)
 }
+//jika nama tidak sama dengan kosong dan value peran sama dengan penyihir
 else if(nama !== "" & peran === "Penyihir"){
 console.log(`Selamat datang di Dunia Proxytia, ${nama}`)
 console.log(`Halo Penyihir ${nama}, ciptakan keajaiban yang membantu kemenanganmu!`)
 }
+//jika nama sama dengan kosong dan peran sama dengan kosong
 else{
 console.log("Nama harus diisi!");
 }
--- exercise4.js
+++ exercise4.js
@@ -1,58 +1,58 @@
+// masukan variabel hari, bulan, tahun
 var hari = 1    ;
 var bulan = 20   ;
 var tahun = 1945;
-
+// lakukan switch pada bulan
 switch(bulan){
-
+// jika variabel bulan = 1, ganti menjadi januari
 case 1:
 console.log(`${hari} Januari ${tahun}`);
 break;
-
+// jika variabel bulan = 2, ganti menjadi februari
 case 2:
 console.log(`${hari} Februari ${tahun}`);
 break;
-
+// jika variabel bulan = 3, ganti menjadi maret
 case 3:
 console.log(`${hari} Maret ${tahun}`);
 break;
-
+// jika variabel bulan = 4, ganti menjadi april
 case 4:
 console.log(`${hari} April ${tahun}`);
 break;
-
+// jika variabel bulan = 5, ganti menjadi mei
 case 5:
 console.log(`${hari} Mei ${tahun}`);
 break;
-
+// jika variabel bulan = 6, ganti menjadi juni
 case 6:
 console.log(`${hari} Juni ${tahun}`);
 break;
-
+// jika variabel bulan = 7, ganti menjadi juli
 case 7:
 console.log(`${hari} Juli ${tahun}`);
 break;
-
+// jika variabel bulan = 8, ganti menjadi agustus
 case 8:
 console.log(`${hari} Agustus ${tahun}`);
 break;
-
+// jika variabel bulan = 9, ganti menjadi september
 case 9:
 console.log(`${hari} September ${tahun}`);
 break;
-
+// jika variabel bulan = 10, ganti menjadi oktober
 case 10:
 console.log(`${hari} Oktober ${tahun}`);
 break;
-
+// jika variabel bulan = 11, ganti menjadi november
 case 11:
 console.log(`${hari} November ${tahun}`);
 break;
-
+// jika variabel bulan = 12, ganti menjadi desember
 case 12:
 console.log(`${hari} Desember ${tahun}`);
 break;
-
-
+// jika variabel bulan bukan 1-12, munculkan "masukan bulan"
 default:
 console.log(`masukan bulan`);
 break;
