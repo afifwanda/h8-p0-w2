677dad430d269af1ba54cfc0b43815dac14a9fa3
Afif Julio <54105946+afifwanda@users.noreply.github.com>
2019-12-21 21:45:00 +0700

    updating code

--- exercise10.js
+++ exercise10.js
@@ -5,9 +5,12 @@ function bandingAngka(angka1, angka2){
         return true;
     }
     // jika angka1 lebih besar dari angka2 munculkan false
-    else {
+    else if (angka1 > angka2) {
         return false;
     }
+    // jika angkanya sama, maka return -1
+    else{
+        return -1}
 }
 
 console.log(bandingAngka(8,2));
